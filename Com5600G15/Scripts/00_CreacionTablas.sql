60b1f17306c8344fdbc4f0c9ac3c9ab0692d0395
Franco Leonel Perez <154629558+Frank-Grimey-Grimes@users.noreply.github.com>
2025-11-01 15:41:57 -0300

    Persona y PersonaUnidad
    Agrego sql de creacion de tablas Persona Y PersonaUnidad

--- Com5600G15/Scripts/00_CreacionTablas.sql
+++ Com5600G15/Scripts/00_CreacionTablas.sql
@@ -103,5 +103,29 @@ CREATE TABLE EstadoFinanciero (
 	CONSTRAINT fk_estadoFinanciero_consorcio FOREIGN KEY (id_consorcio) REFERENCES Consorcio(id_consorcio)
 );
 
+CREATE TABLE PersonaUnidad
+(
+    id_persona_unidad INT IDENTITY(1,1) PRIMARY KEY,
+    id_unidad INT,
+    dni INT,
+    rol CHAR(1) CHECK (rol IN ('P', 'I')), --P = Propietario, I = inquilino
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE NOT NULL,
+    CONSTRAINT FK_PersonaUnidad_Unidad FOREIGN KEY (id_unidad) REFERENCES UnidadFuncional(id_unidad_funcional),
+    CONSTRAINT FK_PersonaUnidad_Persona FOREIGN KEY (dni) REFERENCES Persona(dni)
+)
+GO
+
+CREATE TABLE Persona
+(
+    dni INT PRIMARY KEY,
+    nombre NVARCHAR(50),
+    apellido NVARCHAR(50),
+    mail NVARCHAR(254),
+    telefono VARCHAR(20),
+    tipo_cuenta CHAR(3) CHECK (tipo_cuenta IN ('CBU', 'CVU')),
+    codigo_cuenta CHAR(22) NOT NULL
+)
+GO
 
 
