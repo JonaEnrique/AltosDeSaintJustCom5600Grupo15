2527defdd578949f22743fd135222e1d78806927
Cristian Vergara <vergara0cristian@gmail.com>
2025-11-01 16:13:10 -0300

    Agregacion de FK de tablas

--- Com5600G15/Scripts/00_CreacionTablas.sql
+++ Com5600G15/Scripts/00_CreacionTablas.sql
@@ -38,29 +38,27 @@ GO
 
 CREATE TABLE PagoAsociado (
     id_expensa       INT IDENTITY(1,1) PRIMARY KEY,
-    id_unidadfecha   INT NOT NULL,
-    tipodecuenta     INT NOT NULL,
+    id_unidad   INT NOT NULL,
+	fecha DATE NOT NULL,
+    cvu_cbu     INT NOT NULL,
     codigo_cuenta    INT NOT NULL,
     importe          DECIMAL(10,2) NOT NULL CHECK (importe > 0),
-
     -- Foreign keys
-    FOREIGN KEY (id_unidadfecha) REFERENCES --tabla que no creamos,
-    FOREIGN KEY (tipodecuenta)   REFERENCES --tabla que no creamos,
-    FOREIGN KEY (codigo_cuenta)  REFERENCES --tabla que no creamos
+    FOREIGN KEY (id_unidad) REFERENCES UnidadFuncional(id_unidad),
+    FOREIGN KEY (cbu/cvu)   REFERENCES Persona(tipo_cuenta)
 )
 GO
 
 CREATE TABLE GastoExtraordinario (
     id_gasto        INT IDENTITY(1,1) PRIMARY KEY,
-    id_consorcio    INT NOT NULL, -- FK a agregar después
+    id_consorcio    INT NOT NULL,
     detalle         VARCHAR(255) NOT NULL,
     importe         DECIMAL(10,2) NOT NULL CHECK (importe > 0),
     fecha           DATE NOT NULL,
     pago_cuotas     BIT NOT NULL DEFAULT 0,  -- 0 = no, 1 = sí
     nro_cuota       INT NULL,
     total_cuotas    INT NULL,
-    
-    -- FOREIGN KEY (id_consorcio) REFERENCES Consorcio(id_consorcio)
+    FOREIGN KEY (id_consorcio) REFERENCES Consorcio(id_consorcio)
 )
 GO
 
