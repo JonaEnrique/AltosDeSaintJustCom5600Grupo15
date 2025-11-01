7dd13b7e87411704c3152b0dda6899a52182e38f
Franco Leonel Perez <154629558+Frank-Grimey-Grimes@users.noreply.github.com>
2025-11-01 15:46:07 -0300

    Importacion Json
    Agrego Stored Procedure para importar el archivo Json

--- Com5600G15/Scripts/01_ImportacionArchivos.sql
+++ Com5600G15/Scripts/01_ImportacionArchivos.sql
@@ -103,3 +103,68 @@ BEGIN
     END CATCH
 END;
 GO
+
+
+CREATE OR ALTER PROCEDURE Importacion.ImportarJSON 
+    @RutaArchivo NVARCHAR(4096)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF @RutaArchivo IS NULL OR LTRIM(RTRIM(@RutaArchivo)) = ''
+        BEGIN
+            RAISERROR('La ruta del archivo no puede estar vacía', 16, 1);
+            RETURN;
+        END
+
+    DECLARE @sql NVARCHAR(MAX);
+
+    SET @sql = N'
+    CREATE TABLE ##Consorcios (
+        id NVARCHAR(50),
+        nombre_consorcio NVARCHAR(100),
+        mes NVARCHAR(20),
+        bancarios DECIMAL(18,2),
+        limpieza DECIMAL(18,2),
+        administracion DECIMAL(18,2),
+        seguros DECIMAL(18,2),
+        gastos_generales DECIMAL(18,2),
+        servicios_agua DECIMAL(18,2),
+        servicio_luz DECIMAL(18,2)
+    );
+
+    INSERT INTO ##Consorcios
+    (id, nombre_consorcio, mes, bancarios, limpieza, administracion, seguros, gastos_generales, servicios_agua, servicio_luz)
+    SELECT 
+        id,
+        nombre_consorcio,
+        mes,
+        TRY_CAST(REPLACE(REPLACE(bancarios, ''.'', ''''), '','', ''.'') AS DECIMAL(18,2)),
+        TRY_CAST(REPLACE(REPLACE(limpieza, ''.'', ''''), '','', ''.'') AS DECIMAL(18,2)),
+        TRY_CAST(REPLACE(REPLACE(administracion, ''.'', ''''), '','', ''.'') AS DECIMAL(18,2)),
+        TRY_CAST(REPLACE(REPLACE(seguros, ''.'', ''''), '','', ''.'') AS DECIMAL(18,2)),
+        TRY_CAST(REPLACE(REPLACE(gastos_generales, ''.'', ''''), '','', ''.'') AS DECIMAL(18,2)),
+        TRY_CAST(REPLACE(REPLACE(servicios_agua, ''.'', ''''), '','', ''.'') AS DECIMAL(18,2)),
+        TRY_CAST(REPLACE(REPLACE(servicio_luz, ''.'', ''''), '','', ''.'') AS DECIMAL(18,2))
+    FROM OPENROWSET (
+         BULK ''' + @RutaArchivo + ''',
+         SINGLE_CLOB
+    ) AS j
+    CROSS APPLY OPENJSON(BulkColumn)
+    WITH (
+        id NVARCHAR(50) ''$._id.$oid'',
+        nombre_consorcio NVARCHAR(100) ''$.Nombre del consorcio'',
+        mes NVARCHAR(20) ''$.Mes'',
+        bancarios NVARCHAR(20) ''$.BANCARIOS'',
+        limpieza NVARCHAR(20) ''$.LIMPIEZA'',
+        administracion NVARCHAR(20) ''$.ADMINISTRACION'',
+        seguros NVARCHAR(20) ''$.SEGUROS'',
+        gastos_generales NVARCHAR(20) ''$.GASTOS GENERALES'',
+        servicios_agua NVARCHAR(20) ''$.SERVICIOS PUBLICOS-Agua'',
+        servicio_luz NVARCHAR(20) ''$.SERVICIOS PUBLICOS-Luz''
+    );
+    ';
+
+    EXEC sp_executesql @sql;
+END;
+GO
